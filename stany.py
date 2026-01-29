6e10090145aba0b260b13adf1e6c198c1a9210d7
mikolajDeb <mikolajforaps@gmail.com>
2026-01-30 00:11:33 +0100

    dokonczenie przepisania fizyki na wektory i dodanie potrzebnej fizyki

--- stany.py
+++ stany.py
@@ -3,7 +3,7 @@ import math, pygame
 
 
 
-def idle(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector2, theta):
+def idle(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector2, direction: pygame.math.Vector2):
     veh_acc = a
     const_acc = (0-sila_oporu)/masa_pojazdu
     if v.length() > 0:
@@ -28,7 +28,7 @@ def idle(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector
         "pos": pos
     }
 
-def const_vel(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector2, theta, forward):
+def const_vel(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector2, direction: pygame.math.Vector2, forward):
     veh_acc = a
     is_forward = forward
     if is_forward == True:
@@ -43,16 +43,17 @@ def const_vel(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.V
             veh_acc = pygame.math.Vector2(0,0)
     else:
         veh_acc = pygame.math.Vector2(0,0)
-
+    
     v += veh_acc
     pos.x = pos.x + v.x
     pos.y = pos.y + v.y   
     return {
-        "a": a,
+        "a": veh_acc,
         "v": v,
         "pos": pos
     }
 
+#niepotrzebne funkcja const_vel() załatwia ruch od tyłu
 #def const_backward(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector2, theta):
     veh_acc = a
     const_acc = (sila_napedowa - sila_oporu) / masa_pojazdu
@@ -74,22 +75,93 @@ def const_vel(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.V
         "pos": pos
     }
 
-def steering_left(theta):
-    theta -= 10
-    return theta
+def steering(v: pygame.math.Vector, direction: pygame.math.Vector2, dir: int):
+    #kierunek -1 lewo, 0 prosto, 1 prawo
+    rot_speed = 5
 
-def steering_right(theta):
-    theta += 10
-    return theta
+    if v.length() < 0.5:
+        return{
+            "v": v,
+            "dir": direction
+        }
+    else:
+        #zmiania skretu przy cofaniu
+        if v.dot(direction) < 0:
+            dir = -dir
+            rot_angle = dir * rot_speed
 
-def accelerate(a , v, x, y, theta):
+            v = v.rotate(rot_angle)
+            direction = direction.rotate(rot_angle)
+        #normalne skrecanie
+        else:
+            rot_angle = dir * rot_speed
+
+            v = v.rotate(rot_angle)
+            direction = direction.rotate(rot_angle)
+    
+    return {
+        "v": v,
+        "dir": direction
+    }
+
+
+def accelerate(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector2, direction: pygame.math.Vector2):
+    #Przeliczanie fizyki
+
+    current_v = v.length()
+
+    fall_off_coef = 0.2
+    torque = 1 / math.sqrt((current_v * fall_off_coef) + 1)
+
+    dyn_acc_const = (sila_napedowa - sila_oporu) / masa_pojazdu
+
+    acc_val = torque * dyn_acc_const
+
+    if direction.length() > 0:
+        dir = direction.normalize()
+    else:
+        dir = pygame.math.Vector2(0,0)
+    
+    veh_acc = dir * acc_val
+
+    #aktualizacja fizyki
+
+    a += veh_acc
+
     v += a
-    x = x + v * math.cos(math.radians(theta))
-    y = y + v * math.sin(math.radians(theta))
-    return v, x, y
 
-def brake(a, v, x, y, theta):
-    v -= 2 * a
-    x = x + v * math.cos(math.radians(theta))
-    y = y + v * math.sin(math.radians(theta))
-    return v, x, y
+    pos.x += v.x
+    pos.y += v.y
+
+    return {
+        "a": a,
+        "v": v,
+        "pos": pos
+        }
+
+def brake(a: pygame.math.Vector2, v: pygame.math.Vector2, pos: pygame.math.Vector2, direction: pygame.math.Vector2):
+    veh_acc = a
+    
+
+    brake_acc = abs(2 * (0 - sila_oporu) / masa_pojazdu)
+
+    if v.length() > 0:
+        dir = v.normalize()
+        veh_acc -= dir * brake_acc
+    else:
+        v = pygame.math.Vector2(0,0)
+    
+    stare_v = pygame.math.Vector2(v.x, v.y)
+    v += veh_acc
+
+    if v.length() < 0.1 or v.dot(stare_v) < 0:
+        veh_acc = pygame.math.Vector2(0,0)
+        v = pygame.math.Vector2(0,0)
+
+    pos.x += v.x
+    pos.y += v.y
+    return {
+        "a": veh_acc,
+        "v": v,
+        "pos": pos
+        }
